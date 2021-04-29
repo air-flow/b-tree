3dbe15249041c0da78965f5533ec244556bf0e9b
updraft <updraft.1970@gmail.com>
2021-04-29 19:10:13 +0900

    feat:b_tree create

--- src/Python/experiment.py
+++ src/Python/experiment.py
@@ -1,9 +1,29 @@
 
 
+from _typeshed import NoneType
+
+
+class Item():
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+
+class b_tree(object):
+    def __init__(self):
+        self.root = None
+        self.node = None
+
+    def __inset_item(self, Item):
+        if self.root != None:
+            pass
+        else:
+            self.root = Item
+
+
 if __name__ == "__main__":
 
     max_node = 5  # m = 5
-    
 
     l = list(range(10))
     print(l)
