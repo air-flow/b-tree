f32e34a9cfb65d9c8d7867ac92dfa3d3e6c3a853
updraft <updraft.1970@gmail.com>
2021-04-30 07:24:09 +0900

    feat:root node create

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # b-tree
 
 http://wwwa.pikara.ne.jp/okojisan/b-tree/index.html
+
+key 同値は考えない
--- src/Python/experiment.py
+++ src/Python/experiment.py
@@ -1,8 +1,5 @@
 
 
-from _typeshed import NoneType
-
-
 class Item():
     def __init__(self, key, value):
         self.key = key
@@ -10,27 +7,46 @@ class Item():
 
 
 class Node():
-    def __init__(self):
-        """
-        docstring
-        """
+    def __init__(self, item):
+        self.element_list = [item.key]
+        self.node_list = [item]
+
+    def __insert_node(self, item):
         pass
 
 
-class b_tree(object):
+class b_tree():
     def __init__(self):
         self.root = None
+        self.left_node = None
+        self.right_node = None
 
-    def __inset_item(self, Item):
+    def __inset_root_item(self, item_key, item_value="temp"):
         if self.root != None:
             pass
+            # insert node func call
         else:
-            self.root = Item
+            self.root = Item(item_key, item_value)
+
+    def __insert_node_item(self, select_node, item_key, item_value):
+        if select_node is None:
+            select_node = Node(Item(item_key, item_value))
+        else:
+            # if select_node.key
+            pass
 
 
 if __name__ == "__main__":
-
+    #    5
+    # 1 2  3 4
     max_node = 5  # m = 5
 
-    l = list(range(10))
+    l = list(range(1, 6))
     print(l)
+
+    b = b_tree()
+    # for i in l:
+    # b_tree.__inset_item(i)
+    # print(b)
+    b.__inset_root_item(l[0])
+    
